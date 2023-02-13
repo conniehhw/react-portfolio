e0a2960e28786ca9515148037fefa0cd874fc2a0
Connie Wong <conniehhw@gmail.com>
2023-02-13 16:53:19 -0500

    Add component files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import ProjectsComponent from './Projects';
+// second option: import { ProjectsComponent, Header } from './Projects';
+import Footer from './Footer';
 
-function App() {
+
+//convention to use a CAPITAL to denote 
+function App() { //A functional Component (React) --> returning JSX (using html syntax) //every function we create IS a component
+  //can also include other functions as needed here
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +15,26 @@ function App() {
         <p>
          Hello World, this is my portfolio. My name is Connie.
         </p>
+        <p id='some-tx-tag'>Some Text Here demo CSS
+        </p>
+        <ProjectsComponent />
+        <ProjectsComponent />
+        <ProjectsComponent />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
+//lines 8 - 25 is a top level div/p/section/a, it cannot use siblings, can only have 1 parent element
+//line 18 - ProjectsComponent is injected - from Projects.js
+//line 18 - 20 components are re-usable, bit-size, logic
 
 export default App;
