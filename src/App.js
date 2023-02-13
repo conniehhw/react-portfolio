7a76f559ad85c866a71d0b77a045ca631cd0231e
Connie Wong <conniehhw@gmail.com>
2023-02-13 17:01:26 -0500

    leverage js expressions to render js into JSX, 'name'

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import ProjectsComponent from './Projects';
 import Footer from './Footer';
 
 
+
 //convention to use a CAPITAL to denote 
 function App() { //A functional Component (React) --> returning JSX (using html syntax) //every function we create IS a component
-  //can also include other functions as needed here
+                //can also include other functions as needed here such as js expressions
+
+  const name = 'Connie Wong'; // how to leverage jSccript expressions? how to leverage 'name' into JSX return below?
+                              // for example line 20 where tx is Connie- to render JS into JSX, syntax notation is { }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello World, this is my portfolio. My name is Connie.
+         Hello World, this is my portfolio. My name is {name} Connie. 
         </p>
         <p id='some-tx-tag'>Some Text Here demo CSS
         </p>
